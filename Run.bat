38d7b6f6c10295a11c047e80a653635ab85f09eb
ganganmayuri <wasulemayuri23@gmail.com>
2026-04-01 17:12:58 +0530

    added run.bat file

---
+++ Run.bat
@@ -0,0 +1,2 @@
+ pytest -v -n=2 --html=Html_Reports\OrangeHRM.html --alluredir=Allure_Reports --browser chrome
+ pytest -v -n=2 --html=Html_Reports\OrangeHRM.html --alluredir=Allure_Reports --browser firefox
